62caaed55ddfa01b43bca657e5013013ac7e4a8e
tsukaryu <drgon8otsuka@gmail.com>
2024-10-05 21:23:23 +0900

    feat: escのkeymapを追加

--- init.lua
+++ init.lua
@@ -159,6 +159,7 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+vim.keymap.set('i', 'jk', '<ESC>', { silent = true })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
